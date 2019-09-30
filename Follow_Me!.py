c3ddab6409af6a8612e81ec8be5e683ca3b7b621
Jeremiah Brints <timbothyaracnio@gmail.com>
2019-09-30 15:52:27 -0500

    the math makes noise

--- Follow_Me!.py
+++ Follow_Me!.py
@@ -4,3 +4,9 @@ print("Hello World!")
 print("YO MAMA")
 print("IS A")
 print("VERY NICE WOMAN")
+
+print("maths!")
+print("dogs: ", 3 + 190 - 300 / 20 * 787)
+print("Ist it")
+print(3 + 2 < 5 + 20)
+
